e4ae10ba0e60299124530751d557f9502cfbb568
Pavel Pozdnyak <pozdpl@gmail.com>
2016-06-09 21:45:23 +1200

    Added constants for storage keys

---
+++ src/modules/storage_keys_constants.h
@@ -0,0 +1,13 @@
+/* 
+ * File:   storage_keys_constants.h
+ * Author: pozpl
+ *
+ * Created on June 9, 2016, 7:19 PM
+ */
+
+#pragma once
+
+#define STORAGE_VERSION_KEY  0
+#define STORAGE_VERSION_VALUE  1
+
+#define CURRENT_WORD_INDEX_KEY  1
