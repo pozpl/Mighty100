193484f6a201e4017ec2cca600f196b7b52b7b45
Pavel Pozdnyak <pozdpl@gmail.com>
2016-06-12 11:08:38 +1200

    more robust index building script

--- index_creator.pl
+++ index_creator.pl
@@ -11,24 +11,28 @@ my $endWord = 0;
 my $beginTranslation = 0;
 my $endTranslation = 0;
 open(my $wordsIndex, '>:raw', 'resources/raw/fe_idx') or die "Unable to open: $!";
+my $pipe_found = 0;
 while (($n = read $FILE1, $data, 1) != 0){
     #print "$n bytes read\n";
     $buf .= $data;
     if ($data eq "\n") {
         #print "$data $characterCounter\n";
         $endTranslation = $characterCounter - 1;
-        
-        print $wordsIndex pack('s<',$beginWord) ;
-        print $wordsIndex pack('s<',$endWord) ;
-        print $wordsIndex pack('s<',$beginTranslation) ;
-        print $wordsIndex pack('s<',$endTranslation) ;
+        if($pipe_found){
+            print $wordsIndex pack('s<',$beginWord) ;
+            print $wordsIndex pack('s<',$endWord) ;
+            print $wordsIndex pack('s<',$beginTranslation) ;
+            print $wordsIndex pack('s<',$endTranslation) ;
+        }
         print "$beginWord $endWord $beginTranslation $endTranslation\n";
         $beginWord = $characterCounter+1;
+        $pipe_found = 0;
     }
     if ($data eq "|") {
         #print "$data $characterCounter\n";
         $endWord = $characterCounter - 1;
         $beginTranslation = $characterCounter + 1;
+        $pipe_found = 1;
     }
     $characterCounter++;
     #print "$data\n";
