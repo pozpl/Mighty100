49c9b1f321e52186377cc270a10c1bd9719aa7ff
Pavel Pozdnyak <pozdpl@gmail.com>
2016-06-15 22:27:00 +1200

    Get resources files depending on selected dictionary. Implemented set procedure for dictionary.

--- src/modules/words_dictionary.c
+++ src/modules/words_dictionary.c
@@ -1,29 +1,75 @@
 #include "words_dictionary.h"
 
 
-//static uint16_t *words_index;
 static size_t res_size;
 static uint16_t current_index;
+static uint16_t dictionnary_index;
 
 uint16_t get_last_stored_index();
+void store_last_current_index(uint16_t current_index);
+
+void set_dictionnary(uint16_t new_dictinnary_index) {
+    APP_LOG(APP_LOG_LEVEL_INFO, "SET DICT INDEX %d ", dictionnary_index);
+    if (dictionnary_index != new_dictinnary_index) {
+        current_index = 0;
+        dictionnary_index = new_dictinnary_index;
+
+        store_last_current_index(current_index);
+        persist_write_int(DICTIONARY_INDEX_KEY, (int) dictionnary_index);
+
+        init_words_index();
+    }
+
+}
+
+ResHandle get_index_handler_for_current_dict(uint16_t dictionnary_index) {
+    ResHandle handle;
+    if (dictionnary_index == DICTIONARY_FRENCH_ENG_100_DICT_ID) {
+        handle = resource_get_handle(RESOURCE_ID_INDEX_DATA_FILE);
+    } else  if(dictionnary_index == DICTIONARY_SPANISH_ENG_100_DICT_ID){
+        handle = resource_get_handle(RESOURCE_ID_INDEX_SP_ENG_100);
+    }else{
+        handle = resource_get_handle(RESOURCE_ID_INDEX_DATA_FILE);
+    }
+
+    return handle;
+}
+
+ResHandle get_dictionary_handler_for_current_dict(uint16_t dictionnary_index) {
+    ResHandle handle;
+    if (dictionnary_index == DICTIONARY_FRENCH_ENG_100_DICT_ID) {
+        handle = resource_get_handle(RESOURCE_ID_FR_ENG_FILE);
+    } else  if(dictionnary_index == DICTIONARY_SPANISH_ENG_100_DICT_ID){
+        handle = resource_get_handle(RESOURCE_ID_SP_ENG_100_FILE);
+    }else{
+        handle = resource_get_handle(RESOURCE_ID_FR_ENG_FILE);
+    }
+
+    return handle;
+}
 
 void init_words_index() {
-    //    if (!words_index) {
-    // Get resource handle
-    ResHandle handle = resource_get_handle(RESOURCE_ID_INDEX_DATA_FILE);
+
+    if (persist_exists(DICTIONARY_INDEX_KEY)) {
+        // Read persisted value
+        dictionnary_index = (uint16_t) persist_read_int(DICTIONARY_INDEX_KEY);
+        APP_LOG(APP_LOG_LEVEL_INFO, "READ DICTIONARY INDEX  %d FROM PERSIST", (int)persist_read_int(DICTIONARY_INDEX_KEY));
+    } else {
+        dictionnary_index = DICTIONARY_FRENCH_ENG_100_DICT_ID;
+    }
+    APP_LOG(APP_LOG_LEVEL_INFO, "DICTIONARY INDEX IN INIT %d ", dictionnary_index);
+
+    ResHandle handle = get_index_handler_for_current_dict(dictionnary_index); //resource_get_handle(RESOURCE_ID_INDEX_DATA_FILE);
     // Get size of the resource in bytes
     res_size = resource_size(handle);
-    // Create a buffer the exact size of the raw resource
-    //        words_index = (uint16_t *) malloc(res_size);
-    // Copy all bytes to a buffer
-    //        resource_load(handle, (uint8_t *) words_index, res_size);
-    //    }
+
 
     APP_LOG(APP_LOG_LEVEL_INFO, "WORDS INDEX SIZE IN BYTES %d ", res_size);
     current_index = get_last_stored_index();
     APP_LOG(APP_LOG_LEVEL_INFO, "STORED LAST CURRENT INDEX %d ", current_index);
 
 }
+
 
 //void deinit_words_index() {
 ////    if (words_index) {
@@ -71,10 +117,10 @@ void print_next_word_and_translation(TextLayer *s_word_text_layer, TextLayer *s_
 void print_word_and_translation(uint16_t index, TextLayer *s_word_text_layer, TextLayer *s_translation_text_layer) {
 
     // Get resource handle
-    ResHandle index_handle = resource_get_handle(RESOURCE_ID_INDEX_DATA_FILE);
+    ResHandle index_handle = get_index_handler_for_current_dict(dictionnary_index); //resource_get_handle(RESOURCE_ID_INDEX_DATA_FILE);
     // Get size of the resource in bytes
     res_size = resource_size(index_handle);
-    if ( index * 8 < res_size  ) {
+    if (index * 8 < res_size) {
         // Create a buffer the exact size of the raw resource
         uint16_t *word_index_buffer = (uint16_t *) malloc(4 * sizeof (uint16_t));
         // Copy all bytes to a buffer
@@ -90,7 +136,7 @@ void print_word_and_translation(uint16_t index, TextLayer *s_word_text_layer, Te
 
         uint8_t* word_buffer = (uint8_t *) malloc(end_word - begin_word + 1);
 
-        ResHandle words_resource_handle = resource_get_handle(RESOURCE_ID_FR_ENG_FILE);
+        ResHandle words_resource_handle = get_dictionary_handler_for_current_dict(dictionnary_index); //resource_get_handle(RESOURCE_ID_FR_ENG_FILE);
         resource_load_byte_range(words_resource_handle, begin_word, word_buffer, end_word - begin_word);
 
         word_buffer[end_word - begin_word] = 0;
